e08fb10a36c6ba15b7bfbb617eccfc587e12e28b
unknown <garippolosov@gmail.com>
2024-06-26 17:16:48 +0500

    added getstaticparams

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,26 @@
-const Slug = () =>{
-    return(
-        <h1>About Sadoqat's career</h1>
-    )
-}
-export default Slug;
+// pages/blog/[slug]/page.jsx
+
+// Функция для генерации статических параметров
+export async function generateStaticParams() {
+    return [
+      { slug: 'example' }, // Укажите здесь ваши статические параметры
+    ];
+  }
+  
+  // Функция для получения данных на основе параметров
+  export async function getStaticProps({ params }) {
+    return {
+      props: {
+        slug: params.slug,
+      },
+    };
+  }
+  
+  const Slug = ({ slug }) => {
+    return (
+      <h1>About Sadoqat's career: {slug}</h1>
+    );
+  };
+  
+  export default Slug;
+  
